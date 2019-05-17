30aed6de9c0301b5d7adcfdcda71ab80be12307a
Jhonatan Eduardo <jhonatanepp@gmail.com>
2019-05-17 09:47:25 -0300

    Configura a rota do app 'tasks-list'

--- front-angular/src/app/app-routing.module.ts
+++ front-angular/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { TasksListComponent } from './tasks/tasks-list/tasks-list.component';
+
+const routes: Routes = [
+  { path: 'task/list', component: TasksListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
